cda9b874f7da99dc7ff55c3b46b200c0c9ccb2d3
vladimid <vladimid@gmail.com>
2025-04-03 15:45:05 +0100

    session-2: tasks 8, 9.
    Add internet gateway, NAT gateway, Elastic IP to the deployment

--- session_2/network.tf
+++ session_2/network.tf
@@ -79,3 +79,29 @@ resource "aws_subnet" "subnet_secure_2" {
     Name = format("%s-%s-secure-subnet-2", var.prefix, var.subnet6_cidr)
   }
 }
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id                  = aws_vpc.vpc.id
+  tags = {
+    Name = format("%s-%s-internet-gateway", var.prefix, var.subnet6_cidr)
+  }
+}
+
+resource "aws_eip" "elastic-ip" {
+  domain   = "vpc"
+  tags = {
+    Name = format("%s-%s-elastic-ip", var.prefix, var.subnet6_cidr)
+  }
+}
+
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.elastic-ip.id
+  subnet_id     = aws_subnet.subnet_private_2.id
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.elastic-ip]
+}

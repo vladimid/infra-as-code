c3dc71dae434a8cf32d95823cf721d0eb0deeb3a
vladimid <vladimid@gmail.com>
2025-04-04 14:47:11 +0100

    session-3: Tasks 1-2

--- session_3/backend_support/s3.tf
+++ session_3/backend_support/s3.tf
@@ -1,20 +1,18 @@
-
-resource "aws_s3_bucket" "vld_s3_bucket" {
+# Goal 1 Task 2: Create an S3 Bucket
+resource "aws_s3_bucket" "vld-s3-bucket" {
   bucket = "${var.prefix}-tfstate"
-  # bucket = "vld-iac-lab-tfstate"
   force_destroy = true
-
 }
 
-resource "aws_s3_bucket_versioning" "vld_s3_bucket_versioning" {
-  bucket = aws_s3_bucket.vld_s3_bucket.id
+resource "aws_s3_bucket_versioning" "vld-s3-bucket-versioning" {
+  bucket = aws_s3_bucket.vld-s3-bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_encryption_configuration" "example" {
-  provider = aws.regional
+resource "aws_s3_bucket_server_side_encryption_configuration" "vld-server-side-encryption-configuration" {
+  bucket = aws_s3_bucket.vld-s3-bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -22,6 +20,7 @@ resource "aws_encryption_configuration" "example" {
   }
 }
 
+
 resource "aws_s3_bucket_public_access_block" "vld_s3_bucket_public_access_block" {
-  bucket = aws_s3_bucket.vld_s3_bucket.id
+  bucket = aws_s3_bucket.vld-s3-bucket.id
 }

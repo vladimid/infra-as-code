80fbb18d17b2bfaa16d844c0081c35594a80b5d3
vladimid <vladimid@gmail.com>
2025-04-08 13:52:49 +0100

    Session 4: Route tables refactor

--- session_4/network.tf
+++ session_4/network.tf
@@ -10,18 +10,23 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 
 resource "aws_subnet" "subnets" {
-  for_each = tomap({
-    for subnet in var.subnets : subnet.name => subnet
-  })
+  count = length(var.subnets)
+
+  availability_zone = data.aws_availability_zones.available.names[floor( count.index / 2)]
 
   tags = {
-    Name = format("%s-%s-%s", var.prefix, var.region, each.key)
+    Name = format("%s-%s-%d", var.prefix, data.aws_availability_zones.available.names[floor(count.index / 2)], count.index)
   }
 
   vpc_id = aws_vpc.vpc.id
-  cidr_block = each.value.cidr_block
+  # cidr_block = each.value.cidr_block
+  cidr_block = var.subnets[count.index].cidr_block
 }
 
 
@@ -38,20 +43,23 @@ resource "aws_eip" "elastic-ip" {
     Name = format("%s-elastic-ip", var.prefix)
   }
 }
-#
-# resource "aws_nat_gateway" "nat-gateway" {
-#   allocation_id = aws_eip.elastic-ip.id
-#   subnet_id     = aws_subnet.private_subnet_2.id
-#   tags = {
-#     Name = format("%s-nat-gateway", var.prefix)
-#   }
-#
-#   # To ensure proper ordering, it is recommended to add an explicit dependency
-#   # on the Internet Gateway for the VPC.
-#   depends_on = [aws_eip.elastic-ip]
-# }
+
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.elastic-ip.id
+  subnet_id     = aws_subnet.subnets[3].id
+  tags = {
+    Name = format("%s-nat-gateway", var.prefix)
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.elastic-ip]
+}
 
 
+#################################################################
+########################  ROUTE TABLES  #########################
+#################################################################
 resource "aws_route_table" "public-route-table" {
   vpc_id                  = aws_vpc.vpc.id
   route {
@@ -64,35 +72,30 @@ resource "aws_route_table" "public-route-table" {
   }
 }
 
-# resource "aws_route_table" "private-route-table" {
-#   vpc_id                  = aws_vpc.vpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.nat-gateway.id
-#   }
-#
-#   tags = {
-#     Name = format("%s-route-table", var.prefix)
-#   }
-# }
-#
-#
-# resource "aws_route_table_association" "public_subnet_1" {
-#   subnet_id      = aws_subnet.public_subnet_1.id
-#   route_table_id = aws_route_table.public-route-table.id
-# }
-#
-# resource "aws_route_table_association" "public_subnet_2" {
-#   subnet_id      = aws_subnet.public_subnet_2.id
-#   route_table_id = aws_route_table.public-route-table.id
-# }
-#
-# resource "aws_route_table_association" "private_subnet_1" {
-#   subnet_id      = aws_subnet.private_subnet_1.id
-#   route_table_id = aws_route_table.private-route-table.id
-# }
-#
-# resource "aws_route_table_association" "private_subnet_2" {
-#   subnet_id      = aws_subnet.private_subnet_2.id
-#   route_table_id = aws_route_table.private-route-table.id
-# }
+resource "aws_route_table" "private-route-table" {
+  vpc_id                  = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gateway.id
+  }
+
+  tags = {
+    Name = format("%s-route-table", var.prefix)
+  }
+}
+
+#################################################################
+##################  ROUTE TABLE ASSOCIATIONS  ###################
+#################################################################
+resource "aws_route_table_association" "public_subnets" {
+  count = 2
+  subnet_id      = aws_subnet.subnets[count.index + 1].id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "private_subnets" {
+  count = 2
+  subnet_id      = aws_subnet.subnets[count.index + 3].id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
